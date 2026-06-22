a23f74a1a7b66157272a71fa22c25d8c2bed1b45
Benjamin Guerrero <benguerrerov@duocuc.cl>
2026-06-22 18:00:23 -0400

    feat: logs de despliegue en main para forzar ejecucion de CI

--- front_despacho/src/main.jsx
+++ front_despacho/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import AppRoutes from "./Routes/AppRoutes.jsx";
 import "./index.css";
 
+console.log("Iniciando aplicación frontend - Despliegue CI/CD exitoso");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRoutes />
